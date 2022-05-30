af4b1b2287fcc586b981a6bdd7ce6f7ac7495d5a
Jessica Allman-LaPorte <90071482+JessicaAllman-LaPorte@users.noreply.github.com>
2022-05-30 10:24:06 -0400

    creates introduction PROC and calls from main

--- Proj6_ALLMANLJ.asm
+++ Proj6_ALLMANLJ.asm
@@ -5,6 +5,7 @@ TITLE Project 6: Low-Level I/O Procedures & Macros     (Proj6_ALLMANLJ.asm)
 ; OSU email address: allmanlj@oregonstate.edu
 ; Course number/section:   CS271 Section 400
 ; Project Number: 6                Due Date: 6/5/2022
+; TODO: Update description
 ; Description: 
 
 
@@ -17,6 +18,7 @@ INCLUDE Irvine32.inc
 .data
 
 intro1				BYTE	"Project 6: Fun with Low-Level I/O Procedures & Macros! - by Jessica Allman-LaPorte",13,10,13,10,0
+; TODO: Update description
 intro2				BYTE	"DESCRIPTION LINE 1",13,10
 					BYTE	"DESCRIPTION LINE 2",13,10
 					BYTE	"DESCRIPTION LINE 3",13,10,0
@@ -24,11 +26,34 @@ intro2				BYTE	"DESCRIPTION LINE 1",13,10
 .code
 main PROC
 
-; (insert executable instructions here)
+	PUSH	OFFSET intro1
+	PUSH	OFFSET intro2
+	CALL	introduction
 
 	Invoke ExitProcess,0	; exit to operating system
 main ENDP
 
-; (insert additional procedures here)
+; ---------------------------------------------------------------------
+; Name: introduction
+;
+; Introduces the user to the program and displays the title, programmer's name
+;	and description
+;
+; Preconditions: intro1 and intro2 exist and have been pushed onto stack
+;
+; Postconditions: EDX changed
+; --------------------------------------------------------------------
+introduction PROC
+	PUSH	EBP						; Step 1) Preserve EBP
+	MOV		EBP, ESP				; Step 2) Assign static stack-frame pointer
+	MOV     EDX, [EBP+12]   
+	CALL    WriteString				; print intro1
+	MOV     EDX, [EBP+8]   
+	CALL    WriteString				; print intro2
+	CALL	CrLf
+	
+	POP		EBP
+	RET		8
+introduction ENDP
 
 END main
